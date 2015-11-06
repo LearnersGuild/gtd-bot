9e3ccb34d2804e29dfefedcb067843c12717a4f3
Ania Slimak <anna.slimak@lunarlogic.io>
2015-11-06 17:50:05 +0100

    Fixed using RoleDiff in Strategy::SyncRole

--- app/services/strategies/sync_role.rb
+++ app/services/strategies/sync_role.rb
@@ -1,17 +1,18 @@
 module Strategies
   class SyncRole
-    attr_accessor :glass_frog, :asana, :roles_diff, :roles_saver
+    attr_accessor :glass_frog, :asana, :roles_diff_factory, :roles_saver
 
-    def initialize(glass_frog, asana, roles_diff, roles_saver)
+    def initialize(glass_frog, asana, roles_diff_factory, roles_saver)
       self.glass_frog = glass_frog
       self.asana = asana
-      self.roles_diff = roles_diff
+      self.roles_diff_factory = roles_diff_factory
       self.roles_saver = roles_saver
     end
 
     def perform
       roles = glass_frog.roles
-      diff = roles_diff.diff(roles)
+      roles_diff = roles_diff_factory.new(roles)
+      diff = roles_diff.perform
       asana.update(diff)
       roles_saver.perform(roles)
     end
--- spec/services/strategies/sync_role_spec.rb
+++ spec/services/strategies/sync_role_spec.rb
@@ -1,17 +1,20 @@
 module Strategies
   describe SyncRole do
-    subject { SyncRole.new(glass_frog, asana, roles_diff, roles_saver) }
-    let(:glass_frog) { double(:glass_frog, roles: roles) }
+    subject { SyncRole.new(glass_frog, asana, roles_diff_factory, roles_saver) }
+
+    let(:glass_frog) { instance_double('GlassFrog', roles: roles) }
+    # TODO: make it instance_double after implementing
     let(:asana) { double(:asana, update: true) }
-    let(:roles_diff) { double(:roles_diff, diff: diff) }
-    let(:roles_saver) { double(:roles_saver, perform: true) }
+    let(:roles_diff_factory) { double(:roles_diff_factory, new: roles_diff) }
+    let(:roles_diff) { instance_double('RolesDiff', perform: diff) }
+    let(:roles_saver) { instance_double('RolesSaver', perform: true) }
     let(:roles) { double(:roles) }
     let(:diff) { double(:diff) }
 
     describe "#perform" do
       it "syncs Asana roles with Glass Frog" do
         expect(glass_frog).to receive(:roles)
-        expect(roles_diff).to receive(:diff).with(roles)
+        expect(roles_diff).to receive(:perform)
         expect(asana).to receive(:update).with(diff)
         expect(roles_saver).to receive(:perform).with(roles)
 
