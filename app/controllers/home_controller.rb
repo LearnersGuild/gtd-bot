e297b41dbef5cac38631bc8d1b6594b6cda357bb
Ania Slimak <anna.slimak@lunarlogic.io>
2015-11-09 14:44:46 +0100

    Interface for clearing Asana projects and roles in db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,4 +13,12 @@ class HomeController < ApplicationController
     response = Bot.new(strategies).perform
     render :test_bot, locals: { response: response }
   end
+
+  def clear_cache
+    Role.delete_all
+    asana_client = AsanaClient.new
+    projects = Asana::Project.find_by_team(asana_client.client,
+                                           team: A9n.asana[:team_id])
+    projects.each(&:delete)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :home, controller: :home, only: [:index] do
-    get :test_bot, on: :collection
+    get :test_bot, :clear_cache, on: :collection
   end
 
   root to: "home#index"
