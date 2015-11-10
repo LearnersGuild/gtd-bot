7a48923643b6f8b0aabda43ff1abcaf427feaabc
Ania Slimak <anna.slimak@lunarlogic.io>
2015-11-10 11:16:58 +0100

    Small refactor

--- app/services/asana_roles_updater.rb
+++ app/services/asana_roles_updater.rb
@@ -16,8 +16,7 @@ class AsanaRolesUpdater
   private
 
   def to_create(diff)
-    to_create = diff[:to_create] || []
-    to_create.map do |r|
+    foo(diff[:to_create]) do |r|
       project = asana_client.create_project(ProjectAttributes.new(r.name))
       new_attributes = r.attributes.merge(asana_id: project.id)
       RoleObject.new(new_attributes)
@@ -25,18 +24,23 @@ class AsanaRolesUpdater
   end
 
   def to_delete(diff)
-    to_delete = diff[:to_delete] || []
-    to_delete.map do |r|
+    foo(diff[:to_delete]) do |r|
       asana_client.delete_project(r.asana_id)
       r
     end
   end
 
   def to_update(diff)
-    to_update = diff[:to_update] || []
-    to_update.map do |r|
+    foo(diff[:to_update]) do |r|
       asana_client.update_project(r.asana_id, r.attributes)
       r
+    end
+  end
+
+  def foo(roles, &block)
+    roles ||= []
+    roles.map do |r|
+      block.call(r)
     end
   end
 end
