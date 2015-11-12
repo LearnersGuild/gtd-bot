e0d8c524b5c36626f518746af28c382e648a3816
Ania Slimak <anna.slimak@lunarlogic.io>
2015-11-12 15:35:44 +0100

    Minor spec improvement

--- spec/services/strategies/next_action_task_spec.rb
+++ spec/services/strategies/next_action_task_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 module Strategies
   describe NextActionTask do
     subject { NextActionTask.new(projects_filter, next_action_task_factory) }
-    let(:next_action_task_factory) { double(create: true) }
+    let(:next_action_task_factory) do
+      instance_double('NextActionTaskFactory', create: true)
+    end
     let(:projects_filter) do
       instance_double('ProjectsFilter', without_tasks: projects)
     end
     let(:projects) { [project] }
-    let(:project) { double }
+    let(:project) { ProjectObject.new }
 
     describe '#perform' do
       it 'creates next action task' do
