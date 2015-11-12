a64f61de32c9e96e559a2e07150dfca016286840
Tomasz Balon <tomasz.balon@lunarlogic.io>
2015-11-12 15:34:00 +0100

    updated tests after code review

--- spec/services/strategies/unassigned_task_spec.rb
+++ spec/services/strategies/unassigned_task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module Strategies
   describe UnassignedTask do
-    subject do
+    let(:strategy) do
       UnassignedTask.new(projects_filter, tasks_filter_factory, tasks_assigner)
     end
     let(:projects_filter) do
@@ -12,22 +12,26 @@ module Strategies
       double(:tasks_filter_factory, new: tasks_filter)
     end
     let(:tasks_filter) do
-      instance_double('TasksFilter', unassigned: tasks)
+      instance_double('TasksFilter', unassigned: unassigned_tasks)
     end
     let(:tasks_assigner) { instance_double('TasksAssigner', perform: true) }
     let(:tasks) { [task] }
-    let(:task) { double }
+    let(:task) { TaskObject.new }
     let(:projects) { [project] }
-    let(:project) { ProjectObject.new(tasks: tasks, owner_id: double) }
+    let(:project) { ProjectObject.new(tasks: tasks, owner_id: "77777") }
+    let(:unassigned_tasks) { [unassigned_task] }
+    let(:unassigned_task) { TaskObject.new(asignee: [double]) }
 
     describe '#perform' do
+      subject { strategy.perform }
+
       it 'assigns unnasigned task to project owner' do
         expect(projects_filter).to receive(:with_tasks)
-        expect(project).to receive(:tasks).and_return(tasks)
         expect(tasks_filter_factory).to receive(:new).with(tasks)
         expect(tasks_filter).to receive(:unassigned)
-        expect(tasks_assigner).to receive(:perform)
-        subject.perform
+        expect(tasks_assigner).to receive(:perform).with(unassigned_tasks,
+                                                         project.owner_id)
+        subject
       end
     end
   end
