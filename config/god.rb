596bdfb3aa5d1a864b8aa6101be8a4475ea347aa
Ania Slimak <anna.slimak@lunarlogic.io>
2015-11-17 20:01:29 +0100

    Workers should not be in app because they will get performed on app load...

--- config/god.rb
+++ config/god.rb
@@ -2,7 +2,7 @@ RAILS_ROOT = File.expand_path('../../', __FILE__)
 
 God.watch do |w|
   w.name = "bot"
-  w.start = "ruby #{RAILS_ROOT}/app/workers/bot_worker.rb"
+  w.start = "ruby #{RAILS_ROOT}/workers/bot_worker.rb"
   w.log = "#{RAILS_ROOT}/log/god.log"
 
   w.keepalive
--- workers/bot_worker.rb
+++ workers/bot_worker.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../../config/environment', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 
 class BotWorker < BaseWorker
   inject :strategies_factory, :exception_handler
