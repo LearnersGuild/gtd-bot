2c554007e81627e4920ed388e1919ba808e7296a
Tomasz Balon <tomasz.balon@lunarlogic.io>
2015-11-19 16:27:24 +0100

    fix updating roles names in Asana

--- app/services/roles_saver.rb
+++ app/services/roles_saver.rb
@@ -2,6 +2,7 @@ class RolesSaver
   def perform(diff)
     create_roles(diff[:to_create])
     delete_roles(diff[:to_delete])
+    update_roles(diff[:to_update])
   end
 
   private
@@ -21,5 +22,13 @@ class RolesSaver
   def delete_roles(roles)
     roles ||= []
     Role.where(asana_id: roles.map(&:asana_id)).delete_all
+  end
+
+  def update_roles(roles)
+    roles ||= []
+    roles.each do |role|
+      role_from_db = Role.where(glass_frog_id: role.glass_frog_id).first
+      role_from_db && role_from_db.update(name: role.name)
+    end
   end
 end
