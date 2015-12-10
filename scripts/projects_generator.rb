905fb4827f7788eb6eeb2b0a91f8dce0f4889bd1
Tomasz Balon <tomasz.balon@lunarlogic.io>
2015-12-10 11:30:40 +0100

    implement projects generator script

---
+++ scripts/projects_generator.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../../config/environment', __FILE__)
+
+class ProjectsGenerator < BaseService
+  inject :teams_matcher, :asana_client, :parallel_iterator
+
+  API_KEY = A9n.asana[:api_key]
+  WORKSPACE = A9n.asana[:workspace_id]
+  PROJECTS_NUMBER = 3
+  TASKS_NUMBER = 3
+
+  def perform
+    teams = teams_matcher.perform
+    teams.each do |team|
+      generate_projects(team)
+    end
+  end
+
+  private
+
+  def generate_projects(team)
+    parallel_iterator.each((1..PROJECTS_NUMBER).to_a) do |n|
+      name = "Project_#{n}"
+      project = asana_client.create_project(WORKSPACE, team.asana_id, name: name)
+      generate_tasks(project)
+      logger.info("#{project.name} created")
+    end
+  end
+
+  def generate_tasks(project)
+    (1..TASKS_NUMBER).to_a.each do |t|
+      name = "Task_#{t}_in_#{project.name}"
+      asana_client.create_task(WORKSPACE, project.asana_id, name: name)
+    end
+  end
+end
+ProjectsGenerator.new.perform
