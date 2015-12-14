85a8a9bb5c30e18a88c9ffcbd0a658a47c94163b
Ania Slimak <anna.slimak@lunarlogic.io>
2015-12-14 11:58:08 +0100

    Better strategies order

--- app/services/strategies_factory.rb
+++ app/services/strategies_factory.rb
@@ -16,25 +16,25 @@ class StrategiesFactory < BaseService
 
     [
       injector.sync_role_strategy(projects_repository, team),
+      injector.individual_role_strategy(projects_repository, team),
+      injector.everyone_role_strategy(projects_repository, team),
       injector.clean_projects_names(projects_repository, team),
       injector.next_action_task_strategy(projects_repository,
                                          tasks_repository_factory),
       injector.unassigned_task_strategy(projects_repository,
                                         tasks_repository_factory),
-      injector.individual_role_strategy(projects_repository, team),
-      injector.everyone_role_strategy(projects_repository, team),
+      injector.unassigned_subtask_strategy(projects_repository,
+                                           tasks_repository_factory,
+                                           subtasks_repository_factory),
       injector.assign_role_to_tasks_strategy(projects_repository,
                                              tasks_repository_factory),
       injector.assign_role_task_strategy(projects_repository),
+      injector.everyone_task(projects_repository, team),
       injector.stale_task(projects_repository, tasks_repository_factory,
                           tags_repository, strategies_repository),
       injector.comment_forgotten_tasks(projects_repository,
                                        tasks_repository_factory,
-                                       strategies_repository),
-      injector.everyone_task(projects_repository, team),
-      injector.unassigned_subtask_strategy(projects_repository,
-                                           tasks_repository_factory,
-                                           subtasks_repository_factory)
+                                       strategies_repository)
     ]
   end
 end
