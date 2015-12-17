fec8fa49db48bc67ebb94fb5cb99741babda15d6
Ania Slimak <anna.slimak@lunarlogic.io>
2015-12-17 09:27:28 +0100

    Updated demo server address

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,7 +1,5 @@
 set :branch, ENV.fetch('BRANCH', 'dev')
-server "gtd-bot.demo.llp.pl",
+server "demo-gtd-bot.learnersguild.org",
   user: "lunar",
-  roles: %w{web app db},
-  port: 20_057
-set :port, 20_057
+  roles: %w{web app db}
 set :user, 'lunar'
